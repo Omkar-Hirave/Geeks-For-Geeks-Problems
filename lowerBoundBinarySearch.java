c5971f59c23a5da3e65232c1bfed9eabc3df297a
oi-OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-07-08 09:36:37 +0530

    Add files via upload

---
+++ lowerBoundBinarySearch.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public static int lowerBound(int []nums, int n, int x) {
+       int low = 0 ;
+       int high = n - 1;
+       int ans = n;
+       while(low <= high){
+           int mid = low + (high - low)/2;
+           if(nums[mid] >= x){
+               ans = mid ; 
+               high = mid - 1;
+           }else{
+               low = mid + 1;
+           }
+       }
+       return ans;
+    }
+}
