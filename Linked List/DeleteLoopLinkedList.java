74cd8907a773a55b2238a1d0bd8e04321d7fc43c
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-08-15 18:54:20 +0530

    Add files via upload

---
+++ Linked List/DeleteLoopLinkedList.java
@@ -0,0 +1,29 @@
+class Solution
+{
+    //Function to remove a loop in the linked list.
+    public static void removeLoop(ListNode head){
+        ListNode fast = head ;
+        ListNode slow = head ;
+        
+       while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast){
+                break;
+            }
+        }
+        if(slow == head){
+            while(fast.next != slow){
+                fast = fast.next;
+            }
+            fast.next = null;
+        }else if(slow == fast){
+            slow = head;
+            while(slow.next != fast.next){
+                slow = slow.next;
+                fast = fast.next;    
+            }
+            fast.next = null;
+        }
+    }
+}
