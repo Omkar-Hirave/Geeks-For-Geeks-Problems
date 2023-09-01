88b4463dbee8a38e7c59f824fccfc484cfb26b9f
OmkarHirave <95956716+Omkar-Hirave@users.noreply.github.com>
2023-09-01 21:13:20 +0530

    Add files via upload

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,12 +1,10 @@
 public class PascalTriangle {
     public static void main(String[] args) {
         int numRows = 5;
-        
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numRows - i; j++) {
                 System.out.print("  "); // Add appropriate spacing
             }
-            
             int num = 1;
             for (int j = 0; j <= i; j++) {
                 System.out.print(num + "   "); // Adjust spacing for numbers
